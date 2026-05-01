a6b9bf65b24ef058c145bad7a3d3201e80503d7f
Anthony Hernandez <110367557+Lollollolmymy@users.noreply.github.com>
2026-05-01 11:29:56 -0400

    Update squidget.rb

--- Formula/squidget.rb
+++ Formula/squidget.rb
@@ -6,7 +6,52 @@ class Squidget < Formula
   version "1.0.0"
 
   def install
+    # Install CLI binary
     bin.install "squidget-MacOs" => "squidget"
+
+    # Create .app bundle structure
+    app_dir = prefix/"SquidGet.app"
+    (app_dir/"Contents/MacOS").mkpath
+
+    # Info.plist
+    (app_dir/"Contents/Info.plist").write <<~XML
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>CFBundleName</key><string>SquidGet</string>
+        <key>CFBundleDisplayName</key><string>SquidGet</string>
+        <key>CFBundleExecutable</key><string>squidget-launcher</string>
+        <key>CFBundleIdentifier</key><string>com.lollollolmymy.squidget</string>
+        <key>CFBundleVersion</key><string>1.0.0</string>
+        <key>CFBundleShortVersionString</key><string>1.0.0</string>
+        <key>CFBundlePackageType</key><string>APPL</string>
+      </dict>
+      </plist>
+    XML
+
+    # Launcher script that opens Terminal and runs squidget
+    (app_dir/"Contents/MacOS/squidget-launcher").write <<~SH
+      #!/bin/bash
+      osascript -e 'tell application "Terminal"
+        activate
+        do script "squidget"
+      end tell'
+    SH
+    chmod 0755, app_dir/"Contents/MacOS/squidget-launcher"
+
+    # Symlink .app into /Applications
+    prefix.install_symlink app_dir => "SquidGet.app"
+  end
+
+  def caveats
+    <<~EOS
+      SquidGet has been installed as both a CLI tool and a .app bundle.
+      To add it to your Applications folder, run:
+        cp -r #{prefix}/SquidGet.app /Applications/
+      Or just run it from Terminal:
+        squidget
+    EOS
   end
 
   test do
